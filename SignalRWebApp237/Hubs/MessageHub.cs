890e6f085b0a14d696f996b7d557b820963cbe42
Elvin Camalzade <elvin.camalzade@shawagent.com>
2025-02-27 17:09:44 +0400

    done first

--- SignalRWebApp237/Hubs/MessageHub.cs
+++ SignalRWebApp237/Hubs/MessageHub.cs
@@ -13,5 +13,10 @@ namespace SignalRWebApp237.Hubs
         {
             await Clients.Others.SendAsync("ReceiveDisconnectInfo", "User Disconnected");
         }
+
+        public async Task SendMessage(string message,double data)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", message + "'s Offer is ", data);
+        }
     }
 }
