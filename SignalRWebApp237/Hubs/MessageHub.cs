e9d75a9a6022999ce038672d87117c8eeee4cd69
Elvin Camalzade <elvin.camalzade@shawagent.com>
2025-02-27 16:10:21 +0400

    Add project files.

---
+++ SignalRWebApp237/Hubs/MessageHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRWebApp237.Hubs
+{
+    public class MessageHub:Hub
+    {
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.All.SendAsync("ReceiveConnectInfo", "User Connected");
+        }
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            await Clients.Others.SendAsync("ReceiveDisconnectInfo", "User Disconnected");
+        }
+    }
+}
